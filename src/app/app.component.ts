af0776a27ddaa907702b66263303511df0693e81
Pawel Lesniowski <pawel_lesniowski@epam.com>
2018-02-20 10:45:12 +0100

    getting data from localhost

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  name: string = '';
+  age: number;
+  found: boolean;
+  personProfile = [];
+
+  constructor(private httpClient: HttpClient) {}
+
+  onNameKeyUp(event: any) {
+    this.name = event.target.value;
+    this.found = false;
+    this.personProfile = [];
+  }
+
+  getProfile() {
+    this.httpClient.get(`http://localhost:4002/profiles/?name=${this.name}`)
+      .subscribe(
+        (data: any[]) => {
+          if (data.length) {
+            this.found = true;
+            this.personProfile = data[0];
+          }
+        }
+      );
+  }
+
 }
